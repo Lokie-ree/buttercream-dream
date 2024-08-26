16c9bd07b8f831a9b3993f0c19e35e2050f1b0d9
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-08-26 12:58:55 -0500

    firebase test

--- firebase.js
+++ firebase.js
@@ -1,7 +1,10 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCOAvT8N3UuOc0aNUoiIx-fEjwnK-KQRiQ",
   authDomain: "buttercream-ecom.firebaseapp.com",
@@ -12,9 +15,5 @@ const firebaseConfig = {
   measurementId: "G-WL4FG04FS9",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
--- src/app/components/ui/Navbar.jsx
+++ src/app/components/ui/Navbar.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import React, { useState } from "react";
 import { IoMenuOutline } from "react-icons/io5";
 
