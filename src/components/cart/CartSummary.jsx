1b1fa6772932973e6037422efa37a8cec596ba2c
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-10-31 07:58:45 -0500

    added front end logic to restrict purchases to select zip codes only

--- src/components/cart/CartSummary.jsx
+++ src/components/cart/CartSummary.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const CartSummary = ({ cartItems, subtotal }) => {
+  const [zipCode, setZipCode] = useState("");
   const isCartEmpty = subtotal === 0;
+  const allowedZipCodes = [
+    "70764",
+    "70788",
+    "70346",
+    "70710",
+    "70767",
+    "70737",
+    "70718",
+    "70707",
+  ];
 
   const handleCheckout = async () => {
-    if (cartItems.length === 0) {
+    if (isCartEmpty) {
+      return;
+    }
+
+    // Check if the entered zip code is in the allowed list
+    if (!allowedZipCodes.includes(zipCode)) {
+      alert("Sorry, we only offer local delivery to specific zip codes");
       return;
     }
 
@@ -44,10 +63,21 @@ const CartSummary = ({ cartItems, subtotal }) => {
           Subtotal:{" "}
           <span className="font-semibold">${subtotal.toFixed(2)}</span>
         </p>
+        <label className="text-sm md:text-lg font-semibold text-secondary">
+          Enter Delivery Zip Code
+          <input
+            type="text"
+            value={zipCode}
+            onChange={(e) => setZipCode(e.target.value)}
+            className="input input-accent w-full mt-2 mb-2"
+            placeholder="Enter Zip Code"
+          />
+        </label>
         <div className="card-actions">
           <button
             className="btn btn-accent text-base-100 hover:bg-neutral hover:text-primary w-full"
             onClick={handleCheckout}
+            disabled={isCartEmpty || !zipCode}
           >
             Checkout
           </button>
